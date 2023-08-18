5e5f25672616cf8e952867bf0b20f721c64626f1
Khushi Saini <133095807+khushigaugyann@users.noreply.github.com>
2023-08-18 10:15:17 +0530

    Add files via upload

---
+++ Pattern16.java
@@ -0,0 +1,10 @@
+public class Pattern16 {
+    public static void main(String[] args) {
+        for(int i=70;i>=65;i--){
+            for(int j=65;j<=i;j++){
+                System.out.print((char)j);
+            }
+            System.out.println();
+        }
+    }
+}
